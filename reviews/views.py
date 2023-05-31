cc27fec98fd2762e0da7dff0ba9556f557903ee9
codingMaz <m.balandari.toroghi@gmail.com>
2023-05-31 19:32:37 +0330

    try get method with views

--- reviews/views.py
+++ reviews/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    name = request.GET.get("name") or "world"
+    return HttpResponse(f"Hello, {name}!")
