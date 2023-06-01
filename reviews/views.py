64c905c31f3aa6c92659e7b47bd6098bfbae144e
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-01 14:34:48 +0330

    add a search template and create a function in views to handle search queries

--- reviews/views.py
+++ reviews/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "base.html")
+
+
+def search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search_result.html", {"search_text": search_text})
