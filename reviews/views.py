9aeac78c1c2c7c77fbf95e221a2b3b08abd4bd20
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-07 18:49:09 +0330

    add template directory to the settings and create base.html

--- reviews/views.py
+++ reviews/views.py
@@ -4,8 +4,7 @@ from .models import Book
 
 
 def welcome_view(request):
-    message = f"<html><h1>Welcome to Bookr Maz!</h1><p>{Book.objects.count()} books and counting!</p></html>"
-    return HttpResponse(message)
+    return render(request, "base.html")
 
 
 def index(request):
