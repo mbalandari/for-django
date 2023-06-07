0b4d6224d2850f66a2a74413fd1451ad20e00c4d
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-07 14:27:30 +0330

    create first view and urls via reviews app

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+
+def welcome_view(request):
+    message = f"<html><h1>Welcome to Bookr Maz!</h1><p>{Book.objects.count()} books and counting!</p></html>"
+    return HttpResponse(message)
 
 
 def index(request):
