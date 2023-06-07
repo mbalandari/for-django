216e95bd00bc994a9c8304f638f15b57112b71dc
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-07 19:31:53 +0330

    create template for books list and handle views and urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.welcome_view, name="welcome_view"),
+    path("", views.index, name="index"),
+    path("books/", views.book_list, name="book_list"),
 ]
